aa409863dcb6aa0e13507f8765a3642a901d64ce
Artem Padalka <artpadan@gmail.com>
2023-11-10 15:33:18 +0200

    added some changes in BottomNav.kt

--- app/src/main/java/com/example/fuelconsumptioncalculator/bottomnav/BottomNav.kt
+++ app/src/main/java/com/example/fuelconsumptioncalculator/bottomnav/BottomNav.kt
@@ -1,7 +1,12 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.fuelconsumptioncalculator.bottomnav
 
+import android.annotation.SuppressLint
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
@@ -9,12 +14,17 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavDestination
 import androidx.navigation.NavDestination.Companion.hierarchy
@@ -24,11 +34,18 @@ import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.fuelconsumptioncalculator.ui.theme.Purple40
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun BottomNav() {
     val navController = rememberNavController()
 
-
+    Scaffold(
+        bottomBar = {
+            BottomBar(navController = navController)
+        }
+    ) {
+        BottomNavGraph(navController = navController)
+    }
 }
 
 
@@ -50,6 +67,11 @@ fun BottomBar(navController: NavHostController) {
     )
     {
         screens.forEach { screen ->
+            AddItem(
+                screen = screen,
+                currentRoute = currentRoute,
+                navController = navController
+            )
         }
     }
 
@@ -60,7 +82,7 @@ fun RowScope.AddItem(
     screen: BottomBarScreen,
     currentRoute: NavDestination?,
     navController: NavHostController
-){
+) {
     val selected = currentRoute?.hierarchy?.any { it.route == screen.route } == true
 
     val contentColor =
@@ -69,22 +91,40 @@ fun RowScope.AddItem(
     val backgroundColor =
         if (selected) Purple40.copy(alpha = 0.8f) else Color.Transparent
     Box(
-      modifier = Modifier
-          .height(40.dp)
-          .clip(CircleShape)
-          .background(color = backgroundColor)
-          .clickable(onClick = {
-              navController.navigate(screen.route) {
-                  popUpTo(navController.graph.findStartDestination().id)
-                  launchSingleTop = true
-              }
-          })
+        modifier = Modifier
+            .height(40.dp)
+            .clip(CircleShape)
+            .background(color = backgroundColor)
+            .clickable(onClick = {
+                navController.navigate(screen.route) {
+                    popUpTo(navController.graph.findStartDestination().id)
+                    launchSingleTop = true
+                }
+            })
     ) {
         Row(
             modifier = Modifier
-                .padding(start = 10.dp, end = 10.dp, top = 10.dp, bottom = 8.dp),
+                .padding(
+                    start = 10.dp,
+                    end = 10.dp,
+                    top = 10.dp,
+                    bottom = 8.dp
+                ),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(4.dp)
         ) {
-
+            Icon(
+                painter = painterResource(id = if (selected) screen.icon_focused else screen.icon),
+                contentDescription = "Icon",
+                tint = contentColor
+            )
+            AnimatedVisibility(visible = selected) {
+                Text(
+                    text = screen.title,
+                    color = contentColor
+                )
+            }
         }
     }
 }
+
